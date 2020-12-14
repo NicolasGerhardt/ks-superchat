4895ecfb870ee194f9b14cef70ba2437862e48e0
Gerhardt, Nicolas (IT) <NicolasGerhardt@quickenloans.com>
2020-12-14 15:57:31 -0500

    Added Firebase config to enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBEW6QYPQnyeqLq9E74ddyBAPBgLbZMkCw',
+    authDomain: 'ks-superchat.firebaseapp.com',
+    projectId: 'ks-superchat',
+    storageBucket: 'ks-superchat.appspot.com',
+    messagingSenderId: '935295729914',
+    appId: '1:935295729914:web:6391e360b9c0f6b88d4943',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBEW6QYPQnyeqLq9E74ddyBAPBgLbZMkCw',
+    authDomain: 'ks-superchat.firebaseapp.com',
+    projectId: 'ks-superchat',
+    storageBucket: 'ks-superchat.appspot.com',
+    messagingSenderId: '935295729914',
+    appId: '1:935295729914:web:6391e360b9c0f6b88d4943',
+  },
 };
 
 /*
